186ffc3ab99eb63fbbda50bcc73c84f14684e661
Oscar Garcia <oskarg6@gmail.com>
2022-04-02 20:02:16 +0200

    initial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { BasicStyles } from './Views/Styles/Basic';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={BasicStyles.container}>
+      <Text>Te qiero!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
